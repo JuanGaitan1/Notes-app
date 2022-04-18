2e16c6b9ab52a8b1c3064b886ba16c9c8804de46
Juan Gaitan <diablogaitan2@gmail.com>
2022-04-18 12:58:25 -0300

    Act

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path =require('path');
 const methodOverride = require ('method-override');
 const session = require ('express-session');
-
 //Initiliazations
 const app = express()
 require('./database')
